1f047f7aa51ea09d0061002816a1cff63a66e032
yuta sakamoto <sorigeeeen@gmail.com>
2019-11-04 15:17:27 +0900

    add list page for task

--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'todo.apps.TodoConfig',
+    'bootstrap4',
 ]
 
 MIDDLEWARE = [
@@ -64,6 +65,9 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
+            'builtins':[ 
+                'bootstrap4.templatetags.bootstrap4',
+            ]
         },
     },
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
+from django.views.generic import ListView
+from .models import Task
+
 # Create your views here.
+class TodoListView(ListView):
+    model = Task
+    template_name = "todo/index.html"
+    
+index = TodoListView.as_view()
